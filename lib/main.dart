c5e830d30cfba392fdc1dbd115b17b271ac8405a
kasididd <kasidid.wan@gmail.com>
2023-04-04 13:34:52 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,67 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:scan/scan.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   String qrcode = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    try {
-      platformVersion = await Scan.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Scaffold(
-              appBar: AppBar(
-                title: const Text('Plugin example app'),
-              ),
-              body: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Running on: $_platformVersion\n'),
-                    Wrap(
-                      children: [
-                        ElevatedButton(
-                          child: Text("parse from image"),
-                          onPressed: () async {
-                            XFile? res = await ImagePicker()
-                                .pickImage(source: ImageSource.gallery);
-                            if (res != null) {
-                              String? str = await Scan.parse(res.path);
-                              if (str != null) {
-                                setState(() {
-                                  qrcode = str;
-                                });
-                              }
-                            }
-                          },
-                        ),
-                        ElevatedButton(
-                          child: Text('go scan page'),
-                          onPressed: () {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (_) {
-                              return ScanPage();
-                            }));
-                          },
-                        ),
-                      ],
-                    ),
-                    Text('scan result is $qrcode'),
-                  ],
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Wrap(
+              children: [
+                ElevatedButton(
+                  child: const Text("parse from image"),
+                  onPressed: () async {
+                    XFile? res = await ImagePicker()
+                        .pickImage(source: ImageSource.gallery);
+                    if (res != null) {
+                      String? str = await Scan.parse(res.path);
+                      if (str != null) {
+                        setState(() {
+                          qrcode = str;
+                        });
+                      }
+                    }
+                  },
                 ),
-              ),
+                ElevatedButton(
+                  child: const Text('go scan page'),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return const ScanPage();
+                    }));
+                  },
+                ),
+              ],
             ),
-      },
-    );
+            Text('scan result is $qrcode'),
+          ],
+        ),
+      ),
+    ));
   }
 }
 
@@ -112,7 +82,7 @@ class _ScanPageState extends State<ScanPage> {
     return Scaffold(
       body: WillPopScope(
         onWillPop: () {
-          print("object");
+          controller.pause();
           return Future(() => true);
         },
         child: SizedBox(
@@ -125,8 +95,8 @@ class _ScanPageState extends State<ScanPage> {
                   onPressed: () {
                     controller.toggleTorchMode();
                   },
-                  icon: Icon(Icons.flash_on_rounded),
-                  label: Text("Flash")),
+                  icon: const Icon(Icons.flash_on_rounded),
+                  label: const Text("Flash")),
               Text(qrcode.toString()),
               SizedBox(
                 width: 250,
@@ -135,10 +105,10 @@ class _ScanPageState extends State<ScanPage> {
                   controller: controller,
                   scanAreaScale: .7,
                   scanLineColor: Colors.green.shade400,
-                  onCapture: (data) async {
+                  onCapture: (str) async {
                     setState(() {
-                      if (data.isNotEmpty) {
-                        qrcode = data;
+                      if (str.isNotEmpty) {
+                        qrcode = str;
                         controller.pause();
                         isStop = true;
                       }
@@ -150,10 +120,11 @@ class _ScanPageState extends State<ScanPage> {
                 IconButton(
                     onPressed: () {
                       controller.resume();
-
-                      isStop = false;
+                      setState(() {
+                        isStop = false;
+                      });
                     },
-                    icon: Icon(Icons.play_arrow_rounded)),
+                    icon: const Icon(Icons.play_arrow_rounded)),
             ],
           ),
         ),
